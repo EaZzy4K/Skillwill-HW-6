e39a36eb59a8d35160cac680d8b1cfbbfc6841d4
EaZzy4K <126105844+EaZzy4K@users.noreply.github.com>
2023-05-29 05:10:40 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// #1
+const button = document.querySelector('.btn')
+
+button.addEventListener("click", () => {
+  const modal = document.querySelector('.modal')
+  modal.style.display = 'block'
+})
+
+// #2
