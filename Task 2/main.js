ed8e1673a880644c354fb8e605c872f85b704631
EaZzy4K <126105844+EaZzy4K@users.noreply.github.com>
2023-05-31 23:06:14 +0400

    main.js

---
+++ Task 2/main.js
@@ -0,0 +1,25 @@
+const body = document.body
+const form = document.querySelector(".form")
+const button = document.querySelector(".btn")
+const colorInput = document.querySelector(".input");
+
+const colorsArray = ['red', 'blue', 'green', 'black', 'white']
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  let colorMatchCheck = false
+
+  for(let i = 0; i < colorsArray.length; i++){
+    if(colorInput.value.toLowerCase() === colorsArray[i]) {
+      colorMatchCheck = true
+      body.style.backgroundColor = colorInput.value
+      break;
+    } 
+  }
+
+  if(colorMatchCheck != true){
+    alert('Please choose only colors from the list: Red, Blue, Green, Black, White.')
+  }
+  // colorInput.value = ''
+})
