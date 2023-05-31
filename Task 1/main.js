6b1acf9f462a016fb633deede5785c7f28409280
EaZzy4K <126105844+EaZzy4K@users.noreply.github.com>
2023-05-31 20:05:11 +0400

    main.js

---
+++ Task 1/main.js
@@ -0,0 +1,7 @@
+// #1
+const button = document.querySelector('.btn')
+
+button.addEventListener("click", () => {
+  const modal = document.querySelector('.modal')
+  modal.style.display = 'block'
+})
