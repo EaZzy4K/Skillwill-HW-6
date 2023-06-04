46980e169d9817d78ef03dee80fcf6138d1ef2c4
EaZzy4K <126105844+EaZzy4K@users.noreply.github.com>
2023-06-04 19:59:02 +0400

    Update main.js

--- Task 3/main.js
+++ Task 3/main.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".form")
+const input = document.querySelector(".input")
+const button = document.querySelector(".averageBtn")
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const splitNums = input.value.split(":")
+  let sum = 0
+
+  for(let i = 0; i < splitNums.length; i++){
+    sum = sum + parseInt(splitNums[i])
+  } 
+  const average = sum / splitNums.length
+  alert("The average is:" + " " + average)
+})
